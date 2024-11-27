2ec71ab214c09747202e71ce64e9f9e8a634e876
owaisrafiq05 <owaisrafiq05@gmail.com>
2024-11-27 23:27:54 +0500

    Slightly Optimization

--- lib/Libs.asm
+++ lib/Libs.asm
@@ -6,247 +6,241 @@ print EQU <mWrite>
 .data
 .code
 ; -----------------------------------------------------------------------
-CreateScreen PROC USES eax esi, screen: PTR SCREEN, callback: DWORD
-; Initializes a SCREEN by allocating required resources
-; - Allocates an empty buffer in heap
-; - Creates a sleeping thread pointing to the given function
-; RECEIVES: screen = address of the SCREEN
-;           callback = the executing function of SCREEN
+CreateDisplay PROC USES eax esi, disp: PTR DISPLAY, fnCallback: DWORD
+; Creates a DISPLAY by allocating required resources and setting up a thread
+; RECEIVES: disp = address of DISPLAY
+;           fnCallback = function to be called by thread
 ; RETURNS: nothing
 ; -----------------------------------------------------------------------
     ; type alias:
-    mov esi, screen
-    scr EQU (SCREEN PTR [esi])
+    mov esi, disp
+    dispPtr EQU (DISPLAY PTR [esi])
 
-    ; CREATING THE BUFFER:
-    INVOKE GetProcessHeap   ; get handle for program
-    cmp eax, 0              ; any error?
-    JE _error               ; yes: exit 
-    mov scr.buffer, eax     ; store the handle
-    ; calculate area of screen:
-    mov eax, scr.rows         
-    mul scr.cols
-    mov scr.area, eax 
-    ; allocate memory in heap:
-    INVOKE HeapAlloc, scr.buffer, HEAP_ZERO_MEMORY, scr.area 
-    cmp eax, 0              ; no space left in heap?
-    JE _error               ; yes: exit
-    mov scr.bucket, eax     ; store the pointer to memory
+    ; CREATING BUFFER:
+    INVOKE GetProcessHeap   ; get program heap
+    cmp eax, 0              ; check for error
+    JE _creationError       ; exit if error
+    mov dispPtr.heapHandle, eax ; store heap handle
+    ; calculate screen area:
+    mov eax, dispPtr.rows
+    mul dispPtr.cols
+    mov dispPtr.size, eax
+    ; allocate memory:
+    INVOKE HeapAlloc, dispPtr.heapHandle, HEAP_ZERO_MEMORY, dispPtr.size
+    cmp eax, 0              ; no space available?
+    JE _creationError       ; exit if error
+    mov dispPtr.buffer, eax ; store buffer pointer
 
-    ; CREATING THE THREAD:
-    INVOKE CreateThread, 0, 0, callback, 0, CREATE_SUSPENDED, 0
-    mov scr.thread, eax     ; store the handle
-    JMP proc_end            ; end program
-    _error:
-        print "An error while creating the buffer. Error code = "
+    ; CREATING THREAD:
+    INVOKE CreateThread, 0, 0, fnCallback, 0, CREATE_SUSPENDED, 0
+    mov dispPtr.threadHandle, eax ; store thread handle
+    JMP proc_end            ; finish procedure
+
+    _creationError:
+        print "Error in buffer creation. Code: "
         INVOKE GetLastError
         call WriteDec
         call Crlf
+
     proc_end:
     RET
-CreateScreen ENDP
+CreateDisplay ENDP
 
 ; -----------------------------------------------------------------------
-DeleteScreen PROC, screen: PTR SCREEN
-; Deletes a SCREEN by cleaning up allocated resources
-; - frees the allocated buffer from heap
-; - closes the running thread
-; RECEIVES: screen = address of the SCREEN
+DeleteDisplay PROC, disp: PTR DISPLAY
+; Deletes a DISPLAY by freeing memory and closing the associated thread
+; RECEIVES: disp = address of DISPLAY
 ; RETURNS: nothing
 ; -----------------------------------------------------------------------
-    scr EQU (SCREEN PTR [screen])
-    INVOKE HeapFree, scr.buffer, 0, scr.bucket
-    INVOKE CloseHandle, scr.thread
+    dispPtr EQU (DISPLAY PTR [disp])
+    INVOKE HeapFree, dispPtr.heapHandle, 0, dispPtr.buffer
+    INVOKE CloseHandle, dispPtr.threadHandle
     RET
-DeleteScreen ENDP
+DeleteDisplay ENDP
 
 ; -----------------------------------------------------------------------
-PrintScreen PROC USES eax ecx esi edi edx, screen: PTR SCREEN
-; Prints the contents of given SCREEN on the active buffer
-; RECEIVES: screen = address of SCREEN
+RenderDisplay PROC USES eax ecx esi edi edx, disp: PTR DISPLAY
+; Prints the contents of the DISPLAY buffer to the screen
+; RECEIVES: disp = address of DISPLAY
 ; RETURNS: nothing
 ; -----------------------------------------------------------------------
     ; type alias:
-    mov esi, screen
-    scr EQU (SCREEN PTR [esi])
-    ; move down upto y-offset:
-    mov dh, BYTE PTR [scr.off.y]
-    mov dl, 0 
-    call Gotoxy 
+    mov esi, disp
+    dispPtr EQU (DISPLAY PTR [esi])
 
-    ; display like a regular 2D array:
-    mov edi, scr.bucket
-    mov ecx, scr.rows
-    outer:
-    push ecx
-        ; move left upto x-offset:
-        mov dl, BYTE PTR [scr.off.x]
+    ; Move cursor to the top-left of the screen
+    mov dh, BYTE PTR [dispPtr.offsetY]
+    mov dl, 0
+    call Gotoxy
+
+    ; Print the screen buffer row by row
+    mov edi, dispPtr.buffer
+    mov ecx, dispPtr.rows
+    outerLoop:
+        push ecx
+        ; Move cursor to the x-offset:
+        mov dl, BYTE PTR [dispPtr.offsetX]
         inc dh
         call Gotoxy
 
-        mov ecx, [scr.cols]
-        inner:
+        mov ecx, dispPtr.cols
+        innerLoop:
             mov al, BYTE PTR [edi]
             cmp al, NOCHAR
-            JE _NoChar
+            JE _noChar
                 call WriteChar
-                JMP _next
-            _NoChar:
+                JMP _nextChar
+            _noChar:
                 inc dl
                 call Gotoxy
-            _next:
+            _nextChar:
             inc edi
-        LOOP inner
-        ; call Crlf
-    pop ecx
-    LOOP outer
+        LOOP innerLoop
+        pop ecx
+    LOOP outerLoop
     RET
-PrintScreen ENDP
+RenderDisplay ENDP
 
 ; -----------------------------------------------------------------------
-WriteScreen PROC USES ecx esi edi edx, screen: PTR SCREEN, string: DWORD, strLen: DWORD
-; prints the given string on the buffer
-; ERROR: when there is no room for the given string
-; RECEIVES: screen = address of SCREEN
-;           string = address of the string
-;           strLen = the length of string (without '\0' null char)
+WriteToDisplay PROC USES ecx esi edi edx, disp: PTR DISPLAY, str: DWORD, length: DWORD
+; Writes a string to the display buffer
+; RECEIVES: disp = address of DISPLAY
+;           str = address of the string
+;           length = string length
 ; RETURNS: nothing
 ; -----------------------------------------------------------------------
     ; type alias:
-    mov edx, screen
-    scr EQU (SCREEN PTR [edx])
+    mov edx, disp
+    dispPtr EQU (DISPLAY PTR [edx])
 
-    mov ecx, strLen
-    add ecx, scr.cursor
-    cmp ecx, scr.area   ; cursor exceeds the area?
-    JA _overflow        ; yes: quit
-    
-    mov esi, string     ; esi = source
-    mov edi, scr.cursor
-    add edi, scr.bucket ; edi = destination
-    mov ecx, strLen
-    add scr.cursor, ecx
+    mov ecx, length
+    add ecx, dispPtr.cursor
+    cmp ecx, dispPtr.size   ; check if string fits within the buffer
+    JA _overflowError       ; exit if string exceeds screen size
+
+    mov esi, str     ; source address
+    mov edi, dispPtr.cursor
+    add edi, dispPtr.buffer ; destination address
+    mov ecx, length
+    add dispPtr.cursor, ecx
     cld
     rep movsb
-    JMP end_proc
+    JMP endProc
 
-    _overflow:
-        print "Given string exceed the boundary of screen"
+    _overflowError:
+        print "Error: String exceeds display bounds!"
         call Crlf
-    end_proc:
+
+    endProc:
     RET
-WriteScreen ENDP
+WriteToDisplay ENDP
 
 ; -----------------------------------------------------------------------
-ClearScreen PROC USES ecx esi edi, screen: PTR SCREEN
-; clears the screen by filling buffer with ' ' && by moving cursor to top-left
-; RECEIVES: screen = address of SCREEN
+ClearDisplay PROC USES ecx esi edi, disp: PTR DISPLAY
+; Clears the display buffer by resetting the characters and moving the cursor
+; RECEIVES: disp = address of DISPLAY
 ; RETURNS: nothing
 ; -----------------------------------------------------------------------
     ; type alias:
-    mov esi, screen
-    scr EQU (SCREEN PTR [esi])
+    mov esi, disp
+    dispPtr EQU (DISPLAY PTR [esi])
     
-    mov scr.cursor, 0      ; restart cursor
-    ; fill buffer like a regular 2D array:
-    mov edi, scr.bucket
-    mov ecx, scr.rows
-    outer:
-    push ecx
-        mov ecx, scr.cols
-        inner:
+    mov dispPtr.cursor, 0      ; reset cursor
+    ; Clear screen buffer
+    mov edi, dispPtr.buffer
+    mov ecx, dispPtr.rows
+    outerClear:
+        push ecx
+        mov ecx, dispPtr.cols
+        innerClear:
             mov BYTE PTR [edi], NOCHAR
             inc edi
-        LOOP inner
+        LOOP innerClear
     pop ecx
-    LOOP outer
+    LOOP outerClear
     RET
-ClearScreen ENDP
+ClearDisplay ENDP
 
 ; -----------------------------------------------------------------------
 ; -----------------------------------------------------------------------
 
 ; -----------------------------------------------------------------------
-CopyPlatform PROC USES eax esi edi, destination:PTR PLATFORM, source:PTR PLATFORM
-; Copies contents of 'source' to 'destination'
-; RECEIVES: destination = address of PLATFORM to copy into
-;           source = address of PLATFORM to copy from
+ClonePlatform PROC USES eax esi edi, dest:PTR PLATFORM, src:PTR PLATFORM
+; Copies content from source to destination platform
+; RECEIVES: dest = address of destination PLATFORM
+;           src = address of source PLATFORM
 ; RETURNS: nothing
 ; -----------------------------------------------------------------------
     ; type alias:
-    mov edi, destination
-    dest EQU (PLATFORM PTR [edi])
-    mov esi, source
-    src EQU (PLATFORM PTR [esi])
+    mov edi, dest
+    destPtr EQU (PLATFORM PTR [edi])
+    mov esi, src
+    srcPtr EQU (PLATFORM PTR [esi])
 
-    mov edx, src.Len
-    mov dest.Len, edx
+    mov edx, srcPtr.Len
+    mov destPtr.Len, edx
 
-    mov edx, src.offsetFromLeft
-    mov dest.offsetFromLeft, edx
+    mov edx, srcPtr.offsetFromLeft
+    mov destPtr.offsetFromLeft, edx
 
-    mov edx, src.offsetFromRight
-    mov dest.offsetFromRight, edx
+    mov edx, srcPtr.offsetFromRight
+    mov destPtr.offsetFromRight, edx
 
     RET
-CopyPlatform ENDP
+ClonePlatform ENDP
 
 ; -----------------------------------------------------------------------
-IntToString PROC USES eax edx esi, s: PTR BYTE, n: DWORD
-; Converts string to integer
-; RECEIVES: s = address of string
-;           n = Integer
-; RETURNS: length of string in ecx
-; ----------------------------------------------
+ConvertIntToString PROC USES eax edx esi, str: PTR BYTE, value: DWORD
+; Converts an integer value to a string representation
+; RECEIVES: str = address of output string
+;           value = integer value to convert
+; RETURNS: length of the resulting string
+; -----------------------------------------------------------------------
     LOCAL divisor: DWORD
     mov divisor, 10
-    mov eax, n
-    mov esi, s
+    mov eax, value
+    mov esi, str
     mov ecx, 0
 
-    _while:
+    _conversionLoop:
         mov edx, 0
         DIV divisor
         add dl, 48
         mov BYTE PTR [esi + ecx], dl
         inc ecx
         CMP eax, 0
-        JNE _while
+        JNE _conversionLoop
     
-    _end_while:
-        INVOKE str_reverse, s, ecx
+    _endConversion:
+        INVOKE ReverseString, str, ecx
         RET
-IntToString ENDP
+ConvertIntToString ENDP
 
 ; -----------------------------------------------------------------------
-str_reverse PROC USES eax ecx edx esi edi, s: PTR BYTE, len: DWORD
-; Reverses the string
-; RECEIVES: s = address of string
-;           len = length of string
+ReverseString PROC USES eax ecx edx esi edi, str: PTR BYTE, len: DWORD
+; Reverses the characters in a string
+; RECEIVES: str = address of the string
+;           len = length of the string
 ; RETURNS: nothing
-; ----------------------------------------------
+; -----------------------------------------------------------------------
     LOCAL divisor: DWORD
     mov divisor, 2
     mov eax, len
     mov edx, 0
     DIV divisor
     mov ecx, eax
-    mov esi, s
+    mov esi, str
     mov edi, esi
     add edi, len
     dec edi
 
-    reverse:
+    reverseLoop:
         mov al, BYTE PTR [esi]
         xchg BYTE PTR [edi], al
         mov BYTE PTR [esi], al
         inc esi
         dec edi
-    LOOP reverse
+    LOOP reverseLoop
     RET
-str_reverse ENDP
+ReverseString ENDP
 END
-
-||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
-|||||||||||||||||||||||||||||||| END |||||||||||||||||||||||||||||||||||||||||||||||
-||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
